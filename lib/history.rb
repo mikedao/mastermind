851511d626beaa2f6b0af2f4ab188c983dfc0e57
Michael Dao <michael.dao@gmail.com>
2014-11-14 11:36:54 -0700

    Improved history.rb

--- lib/history.rb
+++ lib/history.rb
@@ -17,13 +17,16 @@ class History
   end
 
   def display(history)
-    title = 'History'
-    outstream.puts top
-    outstream.puts "| #{title.center(30)} |"
-    outstream.puts top
+    display_header
     history.each_with_index do |guess, index|
       outstream.puts "| #{(index+1).to_s.center(14)}| #{guess.upcase.center(14)} |"
     end
+    outstream.puts top
+  end
+
+  def display_header
+    outstream.puts top
+    outstream.puts "| #{"history".center(30)} |"
     outstream.puts top
   end
 
